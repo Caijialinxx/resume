40483570cb3911f862cc44d5d3dd8f8a96590f7a
Caijialinxx <caijialinxx@foxmail.com>
2018-06-10 15:34:04 +0800

    代码 MVC 化

--- js/messagesDB.js
+++ js/messagesDB.js
@@ -1,61 +1,85 @@
 !function () {
-  var APP_ID = 'cJEG2Dk1CraojDVdtANz144V-gzGzoHsz'
-  var APP_KEY = '3gOhg9eS5JbVpoNL19ASOaAT'
+  var view = document.querySelector('section#messages')
 
-  AV.init({
-    appId: APP_ID,
-    appKey: APP_KEY
-  })
-
-  // 获取历史留言
-  var msgQuery = new AV.Query('Messages')
-  msgQuery.find().then(function (msgs) {
-    msgs.forEach(function (msg) {
-      displayMsg(msg)
-    })
-  }, function (error) {
-    console.log(`Error: ${error}`)
-  })
-
-  $('#postMsgForm').submit((e) => {
-    e.preventDefault()
-    let username = $('input[name=username]')[0].value.trim(),
-      content = $('textarea[name=content]')[0].value
-    if (username.length !== 0 && content.trim().length !== 0) {
+  var model = {
+    // 初始化
+    init: function () {
+      let APP_ID = 'cJEG2Dk1CraojDVdtANz144V-gzGzoHsz'
+      let APP_KEY = '3gOhg9eS5JbVpoNL19ASOaAT'
+      AV.init({ appId: APP_ID, appKey: APP_KEY })
+    },
+    // 获取留言数据
+    fetchMessages: function () {
+      let messagesQuery = new AV.Query('Messages')
+      return messagesQuery.find()
+    },
+    // 添加留言数据
+    addMessage: function (username, content) {
       let MessagesDB = AV.Object.extend('Messages')
       let message = new MessagesDB()
-      message.save({
-        'username': username,
-        'content': content
-      }).then(function (msg) {
-        // 刷新并显示新留言
-        displayMsg(msg)
-        // 成功保存之后，执行其他逻辑.
-      }, function (error) {
-        // 异常处理
-        console.error('Failed to create new object, with error message: ' + error.message)
-      })
-    } else {
-      alert('昵称和留言不能为空哦！')
+      console.log(message)
+      return message.save({ 'username': username, 'content': content })
     }
-  })
-
-  function displayMsg(message) {
-    let li = document.createElement('li'),
-      div_username = document.createElement('div'),
-      div_content = document.createElement('div'),
-      div_time = document.createElement('div')
-    
-    div_username.textContent = message.attributes.username
-    div_content.textContent = message.attributes.content
-    div_time.textContent = message.createdAt.toLocaleString()
-
-    div_username.className = 'msg_username'
-    div_content.className = 'msg_content'
-    div_time.className = 'msg_time'
-    // li.innerHTML = `${message.attributes.username}：<br>${message.attributes.content}<span class="time">${message.createdAt.toLocaleString()}</span>`
-    $(li).append(div_username, div_time, div_content)
-    $('#messages > ol').append(li)
   }
 
+  var controller = {
+    view: null,
+    model: null,
+    init: function (view, model) {
+      this.view = view
+      this.model = model
+      this.model.init()
+      this.loadMessages()
+      this.bindEvents()
+    },
+    loadMessages: function () {
+      this.model.fetchMessages().then((msgs) => {
+        msgs.forEach((msg) => {
+          this.displayMessage(msg)
+        })
+      }, function (error) {
+        console.log(`Error: ${error}`)
+      })
+    },
+    bindEvents: function () {
+      $('#postMsgForm').submit((e) => {
+        e.preventDefault()
+        this.submitMessage()
+      })
+    },
+    submitMessage: function () {
+      let username = $('input[name=username]')[0].value.trim(),
+        content = $('textarea[name=content]')[0].value
+      if (username.length !== 0 && content.trim().length !== 0) {
+        this.model.addMessage(username, content).then(function (msg) {
+          // 刷新并显示新留言
+          this.displayMessage(msg)
+        }, function (error) {
+          // 异常处理
+          console.error('Failed to create new object, with error message: ' + error.message)
+        })
+      } else {
+        alert('昵称和留言不能为空哦！')
+      }
+    },
+    displayMessage: function (message) {
+      let li = document.createElement('li'),
+        div_username = document.createElement('div'),
+        div_content = document.createElement('div'),
+        div_time = document.createElement('div')
+      
+      div_username.textContent = message.attributes.username
+      div_content.textContent = message.attributes.content
+      div_time.textContent = message.createdAt.toLocaleString()
+      
+      div_username.className = 'msg_username'
+      div_content.className = 'msg_content'
+      div_time.className = 'msg_time'
+
+      $(li).append(div_username, div_time, div_content)
+      $('#messages > ol').append(li)
+    },
+  }
+
+  controller.init(view, model)  
 }.call()
