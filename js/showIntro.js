86a336e1e974713366779ba08cf648b99d0734ac
Caijialinxx <caijialinxx@foxmail.com>
2018-08-27 21:53:24 +0800

    完善作品集模块

---
+++ js/showIntro.js
@@ -0,0 +1,21 @@
+!function () {
+  let view = View('#portfolio')
+  let controller = Controller({
+    items: null,
+    bindEvents: function () {
+      items = view.querySelectorAll('.item')
+      console.log(items)
+      for (let i = 0; i < items.length; i++) {
+        $(items[i]).mouseenter(this.showIntro.bind(this))
+        $(items[i]).mouseleave(this.hideIntro.bind(this))
+      }
+    },
+    showIntro: function (e) {
+      e.currentTarget.querySelector('.intro-wrapper').style.bottom = '0'
+    },
+    hideIntro: function (e) {
+      e.currentTarget.querySelector('.intro-wrapper').style.bottom = '-150px'
+    }
+  })
+  controller.init(view)
+}.call()
