a8f9f0cae18ace43295c2f19fcd3073429054631
Caijialinxx <caijialinxx@foxmail.com>
2018-08-27 22:22:16 +0800

    改善细节及媒体查询相应变化

--- js/showDetail.js
+++ js/showDetail.js
@@ -7,7 +7,7 @@
       'HTML': `掌握 HTML 元素的使用，理解其语义化标签的作用并能够写出结构清晰的页面。掌握 HTML5 新元素如 canvas 、 nav 及 WebStorage API 等的使用。`,
       'CSS': `掌握 CSS 盒模型、媒体查询等的使用，此外熟练使用 CSS3 动画、转换、过渡、弹性盒子布局、盒阴影等，能够利用 CSS 布局网页及实现精美的效果。`,
       'JavaScript': `掌握 JavaScript ，如作用域、闭包、 DOM 操作等。能够使用 ES6 语法，如 let 、 const 、 箭头函数、模板字面量、解构赋值、模块、类和 Promise 等。`,
-      'jQuery': `熟悉 jQuery 常用 API 的使用，如 Ajax 、 DOM 操作相关、特效、事件等。曾使用原生 JavaScript 封装过简易版的 jQuery.ajax() 。<a href='https://caijialinxx.github.io/homework/ajaxReq/' target='_blank'>查看源码</a>`,
+      'jQuery': `熟悉 jQuery 常用 API 的使用，如 Ajax 、 DOM 操作相关、特效、事件等。曾使用原生 JavaScript 封装过简易版的 jQuery.ajax() 。<a href='https://github.com/Caijialinxx/SendReqByJS' target='_blank'>查看源码</a>`,
       'React & React Native': `使用过 React 和  React Native 分别开发 PC 端和移动端的 ToDo 项目，主要功能有标记完成状态、添加/删除/移动待办及样式的变化。掌握 JSX 、组件、 props 、 state 及生命周期，掌握 React Native 的原生 UI 组件、 React Navigation 、动画等 API 的使用。`,
       'Webpack': `有使用 Webpack 打包项目的经验，了解其管理资源的方法，如 css-loader 、 file-loader 等。`,
       '移动端开发': `会使用 REM 、 vw / vh 、 媒体查询等技术制作适配手机设备的页面，有 React Native 移动端开发经验。`,
@@ -19,7 +19,6 @@
       this.detailContent = view.querySelector('p')
       skillTags = view.querySelectorAll('li')
       for (let i = 0; i < skillTags.length; i++) {
-        console.log(skillTags[i])
         $(skillTags[i]).mouseenter(this.showDetail.bind(this))
       }
     },
